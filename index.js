9946d2513053ee6654db3be07186031b00eab814
phanduchupdkh <48064888+phanduchupdkh@users.noreply.github.com>
2019-12-06 12:04:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 const fetch = require('cross-fetch')
-const username = ''
-const password = ''
-const nodeMailer = require("nodemailer")
-const smtpTransport = require("nodemailer-smtp-transport")
+const username = 'xuanduc'
+const password = '12345678'
 
-const { TelegramClient } = require('messaging-api-telegram');
+const { TelegramClient } = require('messaging-api-telegram')
 // get accessToken from telegram [@BotFather](https://telegram.me/BotFather)
-const client = TelegramClient.connect('972402414:AAE5rvRgp3oanR7tRm7mO2YESRrpE4bya-Q');
-
-
-let transporter = nodeMailer.createTransport(
-  smtpTransport({
-    service: "gmail",
-    auth: {
-      user: "elderlycare.hcmus@gmail.com",
-      pass: "doantn2019"
-    }
-  })
-)
-let mailOptions = {
-  from: "elderlycare.hcmus@gmail.com",
-  to: "phanduchupdkh@gmail.com",
-  subject: `Đặt cơm ${new Date().toISOString()}`,
-  html: ""
-}
+const client = TelegramClient.connect('972402414:AAE5rvRgp3oanR7tRm7mO2YESRrpE4bya-Q')
 
 // login
 
@@ -71,9 +52,14 @@ fetch("https://portal.acexis.com/graphqllunch",
       .then(res => res.json())
       .then(res => {
         let { dishes } = res.data.menuPublishBySite
-
-        const monUaThich = ['cá lóc', 'gà kho', 'canh chua', 'ba rọi kho tiêu', 'canh khổ qua', 'ếch xào cà ri', 'bò xào', 'mực xào cà ri', ]
+        const monKhongUas = ['chiên', 'chay']
+        const monUaThich = ['cá lóc', 'gà kho', 'canh chua', 'ba rọi kho tiêu', 'canh khổ qua', 'ếch xào cà ri', 'bò xào', 'mực xào cà ri',]
         let dish;
+        monKhongUas.forEach(monKhongUa => {
+          dishes = dishes.filter(item => !(item.name.toLowerCase().includes(monKhongUa)))
+        })
+
+        console.log(dishes)
         monUaThich.forEach(mon => {
           if (!dish) {
             dish = dishes.find(item => item.name.toLowerCase().includes(mon))
@@ -100,10 +86,6 @@ fetch("https://portal.acexis.com/graphqllunch",
           .then(res => {
             if (res.data.ordersByUser.length) {
 
-              client.sendMessage(-339081841, 'Đã đặt cơm rồi @phanduchupdkh nên không đặt thêm được nữa ').then(() => {
-                console.log('sent');
-              });
-
               console.log('bạn đã đặt rồi :', res.data.ordersByUser)
             }
             else {
@@ -120,18 +102,10 @@ fetch("https://portal.acexis.com/graphqllunch",
                 })
                 .then(res => {
                   console.log(`ban da dat mon: ${dish.name} thanh cong`)
-                  let html = `
-                    <h3>Dat mon ${dish.name} Thanh cong<h3>
-                  `
-                  mailOptions.html = html
-                  transporter.sendMail(mailOptions, function (err) {
-                    if (err) {
-                      console.log("Sending to failed: " + err)
-                    } else {
-                      console.log("Sent to PXD")
-                      // callback()
-                    }
-                  })
+                  client.sendMessage(-339081841, `@phanduchupdkh ban da dat mon: ${dish.name} thanh cong `).then(() => {
+                    console.log('sent');
+                  });
+
                 })
             }
           })
@@ -143,5 +117,20 @@ fetch("https://portal.acexis.com/graphqllunch",
   })
 
 
-  // Ham huy 
-  // fetch("https://portal.acexis.com/graphqllunch", {"credentials":"omit","headers":{"accept":"*/*","accept-language":"en-US,en;q=0.9","content-type":"application/json","currentsite":"52be5550-be4f-11e9-aa89-2b0626c97f03","sec-fetch-mode":"cors","sec-fetch-site":"same-origin","token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZXIiOiJodHRwOi8vbHVuY2hhcHA0LmRldi5pbyIsInN1YmplY3QiOiI3YTc3OGUwMC1jZmIwLTExZTktYjI1Ni1lNzBlZmUyNmE5ODgiLCJhdWRpZW5jZSI6Inh1YW5kdWMiLCJpYXQiOjE1NzU1MzAzMDAsImV4cCI6MTU3ODEyMjMwMH0.JwASusJLjLksXO9kHP2rQaAqcaD_XCMcBQRnfT3OW18"},"referrer":"https://portal.acexis.com/lun/order","referrerPolicy":"no-referrer-when-downgrade","body":"{\"operationName\":\"orderDishC\",\"variables\":{\"input\":{\"menuId\":\"b61d9d32-172e-11ea-9545-9ff408c0ec7e\",\"dishId\":\"e87b2360-172e-11ea-9545-9ff408c0ec7e\",\"order\":false}},\"query\":\"mutation orderDishC($input: CreateOrderInputC!) {\\n  orderDishC(input: $input)\\n}\\n\"}","method":"POST","mode":"cors"});
+// Ham huy 
+// fetch("https://portal.acexis.com/graphqllunch", {
+//   "credentials": "omit",
+//   "headers": {
+//     "accept": "*/*",
+//     "accept-language": "en-US,en;q=0.9",
+//     "content-type": "application/json",
+//     "currentsite": "52be5550-be4f-11e9-aa89-2b0626c97f03",
+//     "sec-fetch-mode": "cors",
+//     "sec-fetch-site": "same-origin",
+//     "token": "erQaAqcaD_XCMcBQRnfT3OW18"
+//   },
+//   "referrer": "https://portal.acexis.com/lun/order",
+//   "referrerPolicy": "no-referrer-when-downgrade",
+//   "body": "{\"operationName\":\"orderDishC\",\"variables\":{\"input\":{\"menuId\":\"b61d9d32-172e-11ea-9545-9ff408c0ec7e\",\"dishId\":\"e87b2360-172e-11ea-9545-9ff408c0ec7e\",\"order\":false}},\"query\":\"mutation orderDishC($input: CreateOrderInputC!) {\\n  orderDishC(input: $input)\\n}\\n\"}",
+//   "method": "POST", "mode": "cors"
+// });
